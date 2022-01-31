4fcf01595f11ed64a20227e9a37188b4d642e8c1
captaincook-del <cpataincook-del@protonmail.com>
2022-01-31 20:53:20 +0100

    add snake_case2camel_case

--- exemple.py
+++ exemple.py
@@ -9,12 +9,13 @@ __status__ = "Production"
 Exemple fo using utils.py
 """
 
-from utils import load_config
-from utils import c_logger
+from utils import *
 
 if __name__ == "__main__":
     c_logger.info("Exemple start")
     conf = load_config()
     print(conf)
+    print("HelloWorld to : "+camel_case_2snake_case("HelloWorld"))
+    print("hello_world to : "+snake_case_2camel_case("hello_world"))
     c_logger.debug(conf)
     c_logger.info("Exemple end")
